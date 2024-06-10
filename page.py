c94ab507a971774f181a39d101157aa178fb36c7
AbylayDalabay <dalabayabilay@mail.ru>
2024-06-10 09:38:15 +0500

    structurize folders

--- page.py
+++ page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for
 import sqlite3
 import datetime
 import json
@@ -6,7 +6,7 @@ import requests
 import random
 import string
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Function to get table names from a database
 def get_table_names(database):
@@ -47,8 +47,8 @@ def delete_row(database, table, row_id):
 
 # Function to create a backup
 def create_backup(language, table):
-    source_db = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
-    backup_db = 'cards_faq_kz_backup.db' if language == 'kazakh' else 'cards_faq_ru_backup.db'
+    source_db = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
+    backup_db = 'databases/cards_faq_kz_backup.db' if language == 'kazakh' else 'databases/cards_faq_ru_backup.db'
     backup_table = f"{table}_backup_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
     
     conn = sqlite3.connect(backup_db)
@@ -79,8 +79,8 @@ def get_backup_content(database, backup):
 
 # Function to restore from a backup
 def restore_backup(language, table, backup):
-    source_db = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
-    backup_db = 'cards_faq_kz_backup.db' if language == 'kazakh' else 'cards_faq_ru_backup.db'
+    source_db = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
+    backup_db = 'databases/cards_faq_kz_backup.db' if language == 'kazakh' else 'databases/cards_faq_ru_backup.db'
     
     conn = sqlite3.connect(source_db)
     cursor = conn.cursor()
@@ -145,7 +145,7 @@ def index():
 def get_tables():
     data = request.get_json()
     language = data.get('language')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     tables = get_table_names(database)
     return jsonify(tables)
 
@@ -154,7 +154,7 @@ def get_table_content_route():
     data = request.get_json()
     language = data.get('language')
     table = data.get('table')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     columns, content = get_table_content(database, table)
     return jsonify({'columns': columns, 'content': content})
 
@@ -166,7 +166,7 @@ def update_cell_route():
     column = data.get('column')
     row_id = data.get('row_id')
     new_value = data.get('new_value')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     create_backup(language, table)  # Create backup before updating
     update_cell(database, table, column, row_id, new_value)
     return jsonify(success=True)
@@ -177,7 +177,7 @@ def delete_row_route():
     language = data.get('language')
     table = data.get('table')
     row_id = data.get('row_id')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     create_backup(language, table)  # Create backup before deleting
     success = delete_row(database, table, row_id)
     return jsonify(success=success)
@@ -187,7 +187,7 @@ def get_backups_route():
     data = request.get_json()
     language = data.get('language')
     table = data.get('table')
-    database = 'cards_faq_kz_backup.db' if language == 'kazakh' else 'cards_faq_ru_backup.db'
+    database = 'databases/cards_faq_kz_backup.db' if language == 'kazakh' else 'databases/cards_faq_ru_backup.db'
     backups = get_backups(database, table)
     return jsonify(backups)
 
@@ -197,7 +197,7 @@ def get_backup_content_route():
     language = data.get('language')
     table = data.get('table')
     backup = data.get('backup')
-    database = 'cards_faq_kz_backup.db' if language == 'kazakh' else 'cards_faq_ru_backup.db'
+    database = 'databases/cards_faq_kz_backup.db' if language == 'kazakh' else 'databases/cards_faq_ru_backup.db'
     columns, content = get_backup_content(database, backup)
     return jsonify({'columns': columns, 'content': content})
 
@@ -216,7 +216,7 @@ def insert_data_route():
     language = data.get('language')
     table = data.get('table')
     new_data = data.get('data')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     create_backup(language, table)  # Create backup before inserting
     duplicate_found = insert_data_into_table(database, table, new_data)
     return jsonify(success=True, duplicate=duplicate_found)
@@ -226,7 +226,7 @@ def get_last_row_id_route():
     data = request.get_json()
     language = data.get('language')
     table = data.get('table')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     last_row_id = get_last_row_id(database, table)
     return jsonify(last_row_id=last_row_id)
 
@@ -236,7 +236,7 @@ def insert_json_route():
     language = data.get('language')
     table = data.get('table')
     json_data = data.get('data')
-    database = 'cards_faq_kz.db' if language == 'kazakh' else 'cards_faq_ru.db'
+    database = 'databases/cards_faq_kz.db' if language == 'kazakh' else 'databases/cards_faq_ru.db'
     create_backup(language, table)  # Create backup before inserting
 
     valid_entries = []
